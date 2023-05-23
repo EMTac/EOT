3f896b790d8827dcc3f20d3e116408fde9536763
EMTac <115601690+EMTac@users.noreply.github.com>
2023-05-22 20:26:17 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const accessToken = 'pk.eyJ1IjoiZW10YWMiLCJhIjoiY2xoem93bWN4MTd2ODNrbzRzd2pwa2dteCJ9.7ElkbsrYn-0y_lFDvXGlgA';
+const id = 'emtac';
 
 var dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
